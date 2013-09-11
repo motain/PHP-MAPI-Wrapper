7e726aa2e2243fc17a1777633c0640b360bb46db
Anar Alishov <alishov@gmail.com>
2013-09-11 21:18:43 +0200

    Fixes with cache static

--- bc-mapi-cache.php
+++ bc-mapi-cache.php
@@ -4,11 +4,11 @@
  * Brightcove PHP MAPI Wrapper Caching Layer 1.0.0 (12 OCTOBER 2010)
  *
  * REFERENCES:
- *	 Website: http://opensource.brightcove.com
- *	 Source: http://github.com/brightcoveos
+ *     Website: http://opensource.brightcove.com
+ *     Source: http://github.com/brightcoveos
  *
  * AUTHORS:
- *	 Matthew Congrove <mcongrove@brightcove.com>
+ *     Matthew Congrove <mcongrove@brightcove.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the �Software�),
@@ -16,11 +16,11 @@
  * the rights to use, copy, modify, alter, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to
  * whom the Software is furnished to do so, subject to the following conditions:
- *   
+ *
  * 1. The permission granted herein does not extend to commercial use of
  * the Software by entities primarily engaged in providing online video and
  * related services.
- *  
+ *
  * 2. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, SUITABILITY, TITLE,
@@ -29,7 +29,7 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY WHATSOEVER, WHETHER IN AN ACTION OF
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
  * THE SOFTWARE OR THE USE, INABILITY TO USE, OR OTHER DEALINGS IN THE SOFTWARE.
- *  
+ *
  * 3. NONE OF THE AUTHORS, CONTRIBUTORS, NOR BRIGHTCOVE SHALL BE RESPONSIBLE
  * IN ANY MANNER FOR USE OF THE SOFTWARE.  THE SOFTWARE IS PROVIDED FOR YOUR
  * CONVENIENCE AND ANY USE IS SOLELY AT YOUR OWN RISK.  NO MAINTENANCE AND/OR
@@ -38,106 +38,111 @@
 
 class BCMAPICache
 {
-	public static $extension = NULL;
-	public static $location = NULL;
-	public static $memcached = NULL;
-	public static $port = NULL;
-	public static $time = 0;
-	public static $type = NULL;
+    public static $extension = null;
+    public static $location = null;
+    public static $memcached = null;
+    public static $port = null;
+    public static $time = 0;
+    public static $type = null;
 
-	/**
-	 * The constructor for the BCMAPICache class.
-	 * @access Public
-	 * @since 1.0.0
-	 * @param string [$type] The type of caching method to use, either 'file' or 'memcached'
-	 * @param int [$time] How many seconds until cache files are considered cold
-	 * @param string [$location] The absolute path of the cache directory (file) or host (memcached)
-	 * @param string [$extension] The file extension for cache items (file only)
-	 * @param int [$port] The port to use (Memcached only)
-	 */
-	public function __construct($type = 'file', $time = 600, $location, $extension = '.c', $port = 11211)
-	{
-		
-		if(strtolower($type) == 'file')
-		{
-			$type = 'file';
-		} else if(strtolower($type) == 'memcache' || strtolower($type) == 'memcached') {
-			$type = 'memcached';
-			
-			$memcached = new Memcached();
-			$memcached->addServer($location, $port);
-			
-			self::$memcached = $memcached;
-		} else {
-			$type = FALSE;
-		}
-		
-		self::$extension = $extension;
-		self::$location = $location;
-		self::$port = $port;
-		self::$time = $time;
-		self::$type = $type;
-	}
+    /**
+     * The constructor for the BCMAPICache class.
+     * @access Public
+     * @since 1.0.0
+     * @param string [$type] The type of caching method to use, either 'file' or 'memcached'
+     * @param int [$time] How many seconds until cache files are considered cold
+     * @param string [$location] The absolute path of the cache directory (file) or host (memcached)
+     * @param string [$extension] The file extension for cache items (file only)
+     * @param int [$port] The port to use (Memcached only)
+     */
+    public function __construct($type = 'file', $time = 600, $location, $extension = '.c', $port = 11211)
+    {
+        self::init($type, $time, $location, $extension, $port);
+    }
 
-	/**
-	 * Retrieves any valid cached data.
-	 * @access Public
-	 * @since 1.0.1
-	 * @param string [$key] The cache file key
-	 * @return mixed The cached data if valid, otherwise FALSE
-	 */
-	public function get($key)
-	{
-		if(self::$type == 'file')
-		{
-			$file = self::$location . md5($key) . self::$extension;
-	
-			if(file_exists($file) && is_readable($file))
-			{
-				if((time() - filemtime($file)) <= self::$time)
-				{
-					return file_get_contents($file);
-				}
-			}
-			
-			return FALSE;
-		} else if(self::$type == 'memcached') {
-			$data = self::$memcached->get($key);
-			
-			if(self::$memcached->getResultCode() == Memcached::RES_SUCCESS)
-			{
-				return $data;
-			}
-		} else {
-			return FALSE;
-		}
-	}
+    public static function init($type = 'file', $time = 600, $location, $extension = '.c', $port = 11211)
+    {
 
-	/**
-	 * Creates a cache of data.
-	 * @access Public
-	 * @since 1.0.0
-	 * @param string [$key] The cache file key
-	 * @param mixed [$data] The data to cache
-	 */
-	public function set($key, $data)
-	{
-		if(self::$type == 'file')
-		{
-			$file = self::$location . md5($key) . self::$extension;
-	
-			if(is_writable(self::$location))
-			{
-				$handle = fopen($file, 'w');
-				fwrite($handle, json_encode($data));
-				fclose($handle);
-			}
-		} else if(self::$type == 'memcached') {
-			self::$memcached->set($key, $data, time() + self::$time);
-		} else {
-			return FALSE;
-		}
-	}
+        if (strtolower($type) == 'file') {
+            $type = 'file';
+            if (!file_exists($location)) {
+                mkdir($location, 0777, true);
+            }
+        } else {
+            if (strtolower($type) == 'memcache' || strtolower($type) == 'memcached') {
+                $type = 'memcached';
+
+                $memcached = new Memcached();
+                $memcached->addServer($location, $port);
+
+                self::$memcached = $memcached;
+            } else {
+                $type = false;
+            }
+        }
+
+        self::$extension = $extension;
+        self::$location  = $location;
+        self::$port      = $port;
+        self::$time      = $time;
+        self::$type      = $type;
+    }
+
+    /**
+     * Retrieves any valid cached data.
+     * @access Public
+     * @since 1.0.1
+     * @param string [$key] The cache file key
+     * @return mixed The cached data if valid, otherwise FALSE
+     */
+    public static function get($key)
+    {
+        if (self::$type == 'file') {
+            $file = self::$location . md5($key) . self::$extension;
+
+            if (file_exists($file) && is_readable($file)) {
+                if ((time() - filemtime($file)) <= self::$time) {
+                    return file_get_contents($file);
+                }
+            }
+
+            return false;
+        } else {
+            if (self::$type == 'memcached') {
+                $data = self::$memcached->get($key);
+
+                if (self::$memcached->getResultCode() == Memcached::RES_SUCCESS) {
+                    return $data;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Creates a cache of data.
+     * @access Public
+     * @since 1.0.0
+     * @param string [$key] The cache file key
+     * @param mixed [$data] The data to cache
+     */
+    public static function set($key, $data)
+    {
+        if (self::$type == 'file') {
+            $file = self::$location . md5($key) . self::$extension;
+
+            if (is_writable(self::$location)) {
+                $handle = fopen($file, 'w');
+                fwrite($handle, json_encode($data));
+                fclose($handle);
+            }
+        } else {
+            if (self::$type == 'memcached') {
+                self::$memcached->set($key, $data, time() + self::$time);
+            } else {
+                return false;
+            }
+        }
+    }
 }
-
-?>
